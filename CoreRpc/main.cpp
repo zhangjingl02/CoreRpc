da972840afcac359e6c463a5bc549f4303158d89
zhangjing <zhangjing266@gmail.com>
2013-09-17 13:26:46 +0800

    create new project

---
+++ CoreRpc/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Administrator
+ *
+ * Created on 2013年9月17日, 下午1:09
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	//test
+    return 0;
+}
+
