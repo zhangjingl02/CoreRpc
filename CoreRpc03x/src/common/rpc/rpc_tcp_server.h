94e0104eb260a3aadd17a48738af459180a3ce92
zhangjing <zhangjing266@gmail.com>
2013-10-16 17:26:01 +0800

    Rpc

--- CoreRpc03x/src/common/rpc/rpc_tcp_server.h
+++ CoreRpc03x/src/common/rpc/rpc_tcp_server.h
@@ -1,12 +1,17 @@
 #ifndef _H_COMMON_RPC_RPC_TCP_SERVER_H
 #define _H_COMMON_RPC_RPC_TCP_SERVER_H
+#include "../net/message_dispatcher.h"
 #include "../net/tcp_server.h"
+#include "rpc.pb.h"
 namespace rpc{
 
 	class rpc_tcp_server£º public net::tcp_server{
 	
-	
-	
+	public:
+		rpc_tcp_server(net::MessageDispatcher<TransferMessage>* dispatcher,std::size_t pool_size)
+			:dispatcher_(dispatcher),tcp_server(pool_size)
+	private:
+		net::MessageDispatcher<TransferMessage>* dispatcher_;
 	};
 }
 #endif 
