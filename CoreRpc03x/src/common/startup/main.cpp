a6afec27695e7b5d0fb3751b8d936f01bd2d14ee
zhangjing <zhangjing266@gmail.com>
2013-11-13 16:50:07 +0800

    nt

--- CoreRpc03x/src/common/startup/main.cpp
+++ CoreRpc03x/src/common/startup/main.cpp
@@ -1,3 +1,57 @@
+#include "nt_service.h"
+#include <WinBase.h>
+#include <boost/filesystem.hpp>
+#include "../util/file_system.h"
+#ifdef WIN32
+class ShineService : public NTService
+{
+public:
+	ShineService()
+		: NTService("","","")
+	{
+	}
+	virtual bool OnInit()
+	{
+		//loadLoggerConfig();
+		//cacti::AbstractPath::initRols("./apc.conf");
+		//bootupSocket();
+		return true;
+	}
+	virtual void Run()
+	{
+		//Logger& syslog = Logger::getInstance(LOGGER_STARTUP_SYS);
+		
+		//if(m_commander.startService(0))
+		//{
+		//	syslog.info("Running\n");
+		//}
+		NTService::Run();
+	}
+	virtual void OnStop()
+	{
+		//m_commander.stopService();
+		//shutdownSocket();
+	}
+private:
+	//Commander& m_commander;
+};
+
+int doNTService(int argc, char* argv[])
+{
+	std::string modpath = util::FileSystem::getModulePath();
+	SetCurrentDirectory(modpath.c_str());
+
+
+	ShineService ntservice;
+	if (!ntservice.ParseStandardArgs(argc, argv)) {
+		ntservice.StartService();
+	}
+	
+	// 到这里，服务已经停止
+	return ntservice.m_status.dwWin32ExitCode;
+}
+
+#endif
 
 int main(int argc, char** argv) {
 	return 0;
